f097928e169525a566a391933bfe061f242d5862
Sebastian Petersen <sebastian@srmail.dk>
2018-09-26 17:58:15 +0200

    Assignment 5-7

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,17 @@
-// Her skal du skrive din typescriptkode
+// Global properties
+let ul = document.getElementById("listeUsund");
+let inputElement = document.getElementById("input") as HTMLInputElement;
+let btn = document.getElementById("btnAdd");
+let counter = 0;
+
+// Button click event.
+btn.addEventListener("click", () => {
+    if (inputElement.value != null && inputElement.value !== "") {
+        let node = document.createElement("li");
+        node.setAttribute("id", String(counter));
+        node.appendChild(document.createTextNode(inputElement.value));
+        ul.appendChild(node);
+        console.log("Added item with Id: " + counter);
+        counter++;
+    }
+});
